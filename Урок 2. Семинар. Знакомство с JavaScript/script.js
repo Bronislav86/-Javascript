cb2a01dba962af11965a328be64178a7cf43a8d3
Bronislav <shevchenko-bg@yandex.ru>
2024-09-27 22:33:23 +0300

    was added lesson 2 - seminar

---
+++ Урок 2. Семинар. Знакомство с JavaScript/script.js
@@ -0,0 +1,60 @@
+/* Первое задание*/
+
+console.log('Текст из script.js');
+console.log(typeof 'Текст из script.js');
+
+//Второе задание
+let num = 123;
+console.log(num);
+
+let a = 5;
+console.log(a);
+a = 7;
+console.log(a);
+let b = 1 + 2 + 3;
+console.log(b);
+
+// Третье задание
+let c = 10;
+let d = 2;
+console.log(`Создали переменные c = ${c} и d = ${d};`);
+
+let sumNumbers = c + d;
+console.log(sumNumbers);
+
+let differenceOfNumbers = c - d;
+console.log(differenceOfNumbers);
+
+let productOfNumbers = c * d;
+console.log(productOfNumbers);
+
+let divingOfNumbers = c / d;
+console.log(divingOfNumbers);
+
+let resultOfVariables = sumNumbers + differenceOfNumbers + productOfNumbers + divingOfNumbers;
+console.log(resultOfVariables);
+
+//Задание 4 - устное
+
+//Задание 5
+
+let e = 1.5;
+let f = 0.75;
+console.log(e + f);
+
+e = -10;
+console.log(-e);
+
+//Задание 6
+
+let firstName = 'Bronislav';
+let lastName = 'Shevchenko';
+console.log(firstName + ' ' + lastName);
+
+let java = 'java';
+let script = 'script';
+console.log(`${java}${script}`);
+
+let hello = 'hello';
+let world = 'world';
+console.log(`${hello} ${world}`);
